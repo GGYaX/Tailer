98b40bd3774d49b57e7300d27757fbdb64e1d5bd
Yanxin Gong <yanxin.gong22@gmail.com>
2015-07-10 23:20:11 +0200

    modify client

--- client/js/client.js
+++ client/js/client.js
@@ -1,22 +1,51 @@
 var divBegin = '<div>';
 var divEnd = '</div>';
+var defaultPatterns = {
+	'Error':'text-danger',
+	'Debug':'text-info',
+	'Exception':'text-warning'
+};
+var logDiv = $('#logDiv');
 
 $(document).ready(function() {
 	var socket = io.connect('http://localhost:9092');
-	var container = $('#container');
 
 	socket.on('connect', function() {
-		// TODO 
-		var onlineMsg = $(divBegin + 'Ready to read' + divEnd);
-		container.append(onlineMsg);
+		addNewLine('Client connect to server');
 	})
 
 	socket.on('message', function(data) {
-		var newLine = $(divBegin + data + divEnd);
-		container.append(newLine);
+		addNewLine(data);
 	});
 
 	socket.on('disconnect', function() {
-
+		addNewLine('Client disconnect from server');
 	})
 });
+
+function readPattern(argument) {
+	// read a json from a no display p
+	// TODO
+	return defaultPatterns;
+};
+
+function addNewLine(line) {
+	// we set a default pattern
+	var coloredLine = colorLine(line);
+	logDiv.append(turnToJqueryObjet(coloredLine));
+};
+
+function colorLine(line) {
+	var patterns = readPattern();
+	for(var pattern in patterns) {
+		line = divBegin + line.replace(new RegExp(pattern, 'ig'), '<font class="' + defaultPatterns[pattern] + '">' + pattern + '</font>') + divEnd;
+	}
+	return line;
+};
+
+function turnToJqueryObjet(whoami) {
+	if(whoami instanceof jQuery) {
+		return whoami;
+	}
+	return $(whoami);
+};
